5edcc4c2a11636bfac15a76a16755e6e3ad0f55a
oliveiraarturm <oliveiraarturm@hotmail.com>
2026-08-24 15:20:54 -0300

    1st

---
+++ modbus_tcp.py
@@ -0,0 +1,80 @@
+import network
+import time
+from umodbus.tcp import ModbusTCP
+
+# -------------------------
+# Wi-Fi
+# -------------------------
+
+SSID = "Kitnet06"
+PASSWORD = "478288324@"
+
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+
+if not wifi.isconnected():
+    print("Connecting Wi-Fi...")
+    wifi.connect(SSID, PASSWORD)
+
+    while not wifi.isconnected():
+        time.sleep(0.5)
+
+ip = wifi.ifconfig()[0]
+
+print("Connected")
+print("IP:", ip)
+
+
+# -------------------------
+# Modbus TCP
+# -------------------------
+
+modbus = ModbusTCP()
+
+modbus.bind(
+    local_ip=ip,
+    local_port=502
+)
+
+
+# -------------------------
+# Counter
+# -------------------------
+
+counter = 0
+
+
+def register_read(reg_type, address, val):
+    global counter
+
+    counter += 1
+
+    if counter > 65535:
+        counter = 0
+
+    modbus.set_hreg(
+        address=0,
+        value=counter
+    )
+
+    print("Elipse read register:", counter)
+
+
+# Holding Register 0
+modbus.add_hreg(
+    address=0,
+    value=0,
+    on_get_cb=register_read
+)
+
+
+print("Modbus TCP ready")
+print("Holding Register 0")
+
+
+# -------------------------
+# Main loop
+# -------------------------
+
+while True:
+    modbus.process()
